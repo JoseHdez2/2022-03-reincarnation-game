503725edafeab2411b7128d3557dedace8bac387
Jose Hernandez <josemanuelhdez2@gmail.com>
2022-03-19 20:38:30 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Reincarnation Game</h1>
+    </div>
+  );
+}
---
+++ src/ReincarnationGame.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function ReincarnationGame() {
+  let state = {
+    grid: []
+  };
+  return (
+    <div className="App">
+      <h1>Reincarnation Game</h1>
+      <div>asd</div>
+      asd asd
+    </div>
+  );
+}
